f4aa36c402c30fb55da0ad0f595be40450fe7426
sharda2001 <sharda21@navgurukul.org>
2022-03-29 10:51:00 +0530

    first commit

--- models/Profile.js
+++ models/Profile.js
@@ -1,39 +1,3 @@
-// const mongoose=require('mongoose');
-
-// const profileSchema= new mongoose.Schema({
-//     user:{
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'user'
-//     },
-//     company:{
-
-//         type:String
-//     },
-//     website:{
-//         type:String
-//     },
-//     location:{
-//         type:String
-//     },
-//     status:{
-//         type:String,
-//         required:true
-//     },
-//     skills:{
-//         type:[String],
-//         required:true
-//     },
-//     bio:{
-//         type:String
-//     },
-//     githubusername:{
-//         type:String
-//     },
-
-// });
-
-// module.exports=Profile=mongoose.model('profile'.profileSchema)
-
 const mongoose = require("mongoose");
 
 const ProfileSchema = new mongoose.Schema({
